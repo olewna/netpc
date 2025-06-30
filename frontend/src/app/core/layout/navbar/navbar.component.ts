5e4b55ac8ce3f1d308b730bbc4ba7199ac39da22
olewna <dajcie.suba2002@gmail.com>
2025-06-30 13:15:53 +0200

    navbar

--- frontend/src/app/core/layout/navbar/navbar.component.ts
+++ frontend/src/app/core/layout/navbar/navbar.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  public constructor(private router: Router) {}
 
+  public goToHome() {
+    this.router.navigate(['home']);
+  }
+
+  // public goToLogin() {
+  //   this.router.navigate(['login'])
+  // }
 }
